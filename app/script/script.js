c441130acac974849659d4f702b4ebb40f8d6207
Pavel Siarebrany <pavel97107@gmail.com>
2020-01-25 14:49:12 +0300

    Выполнил второй урок!

--- app/script/script.js
+++ app/script/script.js
@@ -1,11 +1,23 @@
-let money = 'hi gays';
-let income = 6;
-let deposit = 3;
-let mission = 4;
-let addExpenses = 5;
-let period = 6;
+let money = 1000;
+let income = 'фриланс';
+let deposit = true;
+let mission = 10000;
+let addExpenses = 'спорт, курсы, интернет';
+let period = 12;
 
-console.log('heloo world');
+console.log(typeof 1000);
+console.log(typeof 'фриланс');
+console.log(typeof true);
+console.log(addExpenses.length);
 
-alert(money);
+console.log('Период равен 12 месяцев' + " и " + 'Цель заработать 10000 белорусских рублей.');
+
+console.log(addExpenses.toLowerCase());
+console.log(addExpenses.split(', '));
+
+let budgetDay = 100;
+console.log(budgetDay);
+
+
+ 
 
