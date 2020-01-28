24c1c6d1b25a3a725bdcbd0427bb305cde61771c
Pavel Siarebrany <pavel97107@gmail.com>
2020-01-28 19:45:49 +0300

    Посмотрел урок№4 сделал записи

--- app/script/script.js
+++ app/script/script.js
@@ -1,6 +1,6 @@
 'use strick';
 
-let money = prompt('Ваш месячный доход?', '1000');
+let money = +prompt('Ваш месячный доход?', 1000);
 console.log('money: ', money);
 
 let income = 'фриланс';
@@ -13,40 +13,113 @@ console.log('mission: ', mission);
 
 
 let addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую',
-['спорт, курсы, интернет']);
+    ['спорт, курсы, интернет']);
 console.log(addExpenses.split(', '));
 
-let period = 12;
-
 let expenses1 = prompt('Введите обязательную статью расходов?');
-console.log('expenses1: ', expenses1);
+console.log('Статья расходов: ', expenses1);
 
 let expenses2 = prompt('Введите обязательную статью расходов?');
-console.log('expenses2: ', expenses2);
+console.log('Статься расходов: ', expenses2);
 
 let amount1 = prompt('Во сколько это обойдется?');
-console.log('amount1: ', amount1);
+console.log('Обойдется: ', amount1);
 
 let amount2 = prompt('Во сколько это обойдется?');
-console.log('amount2: ', amount2);
+console.log('Обойдется: ', amount2);
 
-let budgetMonth = money - amount1 - amount2;
+let budgetMonth = money - (amount1) - (amount2);
 console.log(budgetMonth);
-console.log(Math.floor(mission / budgetMonth));
 
-let budgetDay = Math.round (budgetMonth / 30); 
-console.log('budgetDay: ', budgetDay);
+let period = Math.ceil(mission / budgetMonth);
+console.log('Цель будет достигнута за ' + (period) + ' месяца');
 
-if (budgetDay >= 1200) {
-    console.log('У вас высокий уровень дохода');
-} else if (budgetDay > 600) {
-    console.log('У вас средний уровень дохода');
-} else {
-    console.log('К сожалению у вас уровень дохода ниже среднего');
-} if (budgetDay < 0) {
-    console.log('Что то пошло нет так');
+let budgetDay = Math.round(budgetMonth / 30);
+console.log('Бюджет на день', budgetDay);
+
+let getIncome = function () {
+    if (budgetDay >= 1200) {
+        return ('У вас высокий уровень дохода');
+    } else if (budgetDay > 600) {
+        return ('У вас средний уровень дохода');
+    } else {
+        return ('К сожалению у вас уровень дохода ниже среднего');
+    }
+    if (budgetDay < 0) {
+        return ('Что то пошло нет так');
+    }
+};
+console.log(getIncome());
+
+// if (budgetDay >= 1200) {
+//     console.log('У вас высокий уровень дохода');
+// } else if (budgetDay > 600) {
+//     console.log('У вас средний уровень дохода');
+// } else {
+//     console.log('К сожалению у вас уровень дохода ниже среднего');
+// } if (budgetDay < 0) {
+//     console.log('Что то пошло нет так');
+// }
+
+
+///////////////////////////////////////////////////////////////////////////////
+//Одна функция - одно действие!
+
+
+function outputMessage(name, age) {
+    console.log('Hello ' + name);
+    console.log('My name ' + age);
 }
+outputMessage('Max', 30); // способы вызывать функию стандартный Способ function declaration
+
+
+let f = 0;
+
+const sum = function (a, b) {
+    console.log(arguments); // псевдо массив
+    f = a + b;
+};
+sum(1, 3, 3, 4, 5, 5);
+console.log(f);
+
+/////////////// Чистая функция
+
+const sun = function (a, b) {
+    return a + b;
+};
+
+let fek = sun(3, 4);
+console.log(fek);
+
+
+const suk = function (a, b) {
+    if (a) {
+        a = 0;
+    }
+
+    if (!b) {
+        b = 0;
+    }
+
+    return a + b; //возвращает аргументы в переменную а также return отстанавливает дальнейшее применение функции
+};
+
+let mn = suk(5);
+console.log(mn);
+
+let outShow = function (data) {
+    console.log(data, typeof (data));
+
+};
+outShow(money);
+outShow(income);
+outShow(deposit);
 
 
 
+// const userName = function(){
+//     console.log('Gello world');
+// };
+// userName();  // через переменную Способ function expression
 
+// const userLog = new Function('alert("Hi")'); // функция через конструктор
