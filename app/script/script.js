1f0e5975a5e79f6812f469c82f57d4d957b4cb56
Pavel Siarebrany <pavel97107@gmail.com>
2020-01-28 17:42:22 +0300

    Сделал усложненное задание

--- app/script/script.js
+++ app/script/script.js
@@ -37,7 +37,7 @@ console.log(Math.floor(mission / budgetMonth));
 let budgetDay = Math.round (budgetMonth / 30); 
 console.log('budgetDay: ', budgetDay);
 
-if (budgetDay > 1200) {
+if (budgetDay >= 1200) {
     console.log('У вас высокий уровень дохода');
 } else if (budgetDay > 600) {
     console.log('У вас средний уровень дохода');
@@ -47,9 +47,12 @@ if (budgetDay > 1200) {
     console.log('Что то пошло нет так');
 }
 
+let namePerson = 'Слава';
 
+let result = namePerson === 'Артем' ? 'Директор' : 'Студент';
+console.log('result: ', result);
 
-
-
+let result2 = namePerson === 'Максим' ? 'Преподователь' : 'Студент';
+console.log('result2: ', result2);
 
 
