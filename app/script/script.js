bd7b96b4ecca8a6706bac462345d2edc0dd650b3
Pavel Siarebrany <pavel97107@gmail.com>
2020-01-28 21:07:20 +0300

    Удалили переменную budgetMonth

--- app/script/script.js
+++ app/script/script.js
@@ -1,6 +1,6 @@
 'use strick';
 
-let money = +prompt('Ваш месячный доход?', 1000);
+let money = +prompt('Ваш месячный доход?', 15000);
 console.log('money: ', money);
 
 let income = 'фриланс';
@@ -8,7 +8,7 @@ let deposit = confirm('Есть ли у вас депозит в банке?');
 
 console.log('deposit: ', deposit);
 
-let mission = prompt('Цель заработать?', '10000');
+let mission = +prompt('Цель заработать?', 30000);
 console.log('mission: ', mission);
 
 
@@ -16,20 +16,18 @@ let addExpenses = prompt('Перечислите возможные расход
     ['спорт, курсы, интернет']);
 console.log(addExpenses.split(', '));
 
-let expenses1 = prompt('Введите обязательную статью расходов?');
+let expenses1 = prompt('Введите обязательную статью расходов?', 'eda');
 console.log('Статья расходов: ', expenses1);
 
-let expenses2 = prompt('Введите обязательную статью расходов?');
+let expenses2 = prompt('Введите обязательную статью расходов?', 'eda');
 console.log('Статься расходов: ', expenses2);
 
-let amount1 = prompt('Во сколько это обойдется?');
+let amount1 = +prompt('Во сколько это обойдется?' , 3500);
 console.log('Обойдется: ', amount1);
 
-let amount2 = prompt('Во сколько это обойдется?');
+let amount2 = +prompt('Во сколько это обойдется?', 4500);
 console.log('Обойдется: ', amount2);
 
-let budgetMonth = money - (amount1) - (amount2);
-console.log(budgetMonth);
 
 let period = Math.ceil(mission / budgetMonth);
 console.log('Цель будет достигнута за ' + (period) + ' месяца');
@@ -51,75 +49,23 @@ let getIncome = function () {
 };
 console.log(getIncome());
 
-// if (budgetDay >= 1200) {
-//     console.log('У вас высокий уровень дохода');
-// } else if (budgetDay > 600) {
-//     console.log('У вас средний уровень дохода');
-// } else {
-//     console.log('К сожалению у вас уровень дохода ниже среднего');
-// } if (budgetDay < 0) {
-//     console.log('Что то пошло нет так');
-// }
-
-
-///////////////////////////////////////////////////////////////////////////////
-//Одна функция - одно действие!
-
-
-function outputMessage(name, age) {
-    console.log('Hello ' + name);
-    console.log('My name ' + age);
+function getExpensesMonth(Allamount) {
+    console.log(Allamount); 
 }
-outputMessage('Max', 30); // способы вызывать функию стандартный Способ function declaration
-
-
-let f = 0;
-
-const sum = function (a, b) {
-    console.log(arguments); // псевдо массив
-    f = a + b;
-};
-sum(1, 3, 3, 4, 5, 5);
-console.log(f);
-
-/////////////// Чистая функция
-
-const sun = function (a, b) {
-    return a + b;
-};
-
-let fek = sun(3, 4);
-console.log(fek);
-
-
-const suk = function (a, b) {
-    if (a) {
-        a = 0;
-    }
-
-    if (!b) {
-        b = 0;
-    }
-
-    return a + b; //возвращает аргументы в переменную а также return отстанавливает дальнейшее применение функции
-};
-
-let mn = suk(5);
-console.log(mn);
-
-let outShow = function (data) {
-    console.log(data, typeof (data));
-
-};
-outShow(money);
-outShow(income);
-outShow(deposit);
+getExpensesMonth(amount1 + amount2); // сумма рассходов
 
 
 
-// const userName = function(){
-//     console.log('Gello world');
-// };
-// userName();  // через переменную Способ function expression
 
-// const userLog = new Function('alert("Hi")'); // функция через конструктор
+function getAccumulatedMonth() {
+    return (money - (amount1 + amount2)); 
+}
+getAccumulatedMonth(); // возвращает сумму накопления (доходы-расходы)
+
+let accumulatedMonth = getAccumulatedMonth();
+console.log('Месячное накопление', accumulatedMonth);
+
+function getTargetMonth(a, b) {
+    console.log(Math.floor(a / b));
+}
+getTargetMonth(mission, accumulatedMonth);
