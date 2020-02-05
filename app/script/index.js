832d9cd749d5f411167eab34f28dddc850bb1f60
Pavel Siarebrany <pavel97107@gmail.com>
2020-02-05 20:04:31 +0300

    lesson9-commit#2

---
+++ app/script/index.js
@@ -0,0 +1,19 @@
+'use strict';
+
+let buttonStart = document.getElementById('start'),
+    buttonPlus = document.getElementsByTagName('button' [0]),
+    TwoButtonPlus = document.getElementsByTagName('button' [1]),
+    checkBox = document.querySelector('#deposit-check'),
+    addIncomeItem = document.querySelectorAll('.additional_income'),
+    OneElemAddIncome = document.querySelectorAll('.additional_income' [1]),
+    TwoElemAddIncome = document.querySelectorAll('.additional_income' [2]);
+
+
+let resultBudgetMonth = document.getElementsByClassName('.budget_month-value'),
+    resultBudgetDay = document.getElementsByClassName('.budget_day-value'),
+    resultEXpensesMonth = document.getElementsByClassName('.expenses_month-value'),
+    resultAdditionalIncome = document.getElementsByClassName('.additional_income-value'),
+    resultAdditionalExpenses = document.getElementsByClassName('.additional_expenses-value'),
+    resultIncomePeriod = document.getElementsByClassName('.income_period-value'),
+    resultTargetMonth = document.getElementsByClassName('.target_month-value');
+
